39beea2172bc7c9fce724a0e6ecef6f892e1525b
Aman Kocharekar <aman@Amans-MacBook-Air.local>
2025-04-09 21:56:59 +0530

    do this

--- main.py
+++ main.py
@@ -151,3 +151,8 @@ async def recommend_assessments(req: QueryRequest):
     return {
         "recommended_assessments": records[:10]
     }
+
+
+if __name__ == "__main__":
+    import uvicorn
+    uvicorn.run("main:app", host="0.0.0.0", port=8080)
